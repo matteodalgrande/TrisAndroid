a0bd9e2c980e2349a268acefd59f4eed67957cec
matteodalgrande <matteodalgrande97@gmail.com>
2020-03-22 19:27:36 +0100

    cercando di stampare id dei bottoni

--- app/src/main/java/com/example/trisandroid/MainActivity.java
+++ app/src/main/java/com/example/trisandroid/MainActivity.java
@@ -5,29 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private String giocatore = "primo";
+    private String[] tabella = new String[9];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void click(View view){
+        Button vista = (Button) view;
+        System.out.println(vista.getId());
+    }
+//    public void click(View view) {
+//        if (giocatore == "primo"){
+//            //turno primo giocatore
+//            Button vista = (Button) view;
+//
+//            //andiamo a decrementare id, perchè gli array partono poi da zero
+//            int id = view.getId().split("tabella").toInt() - 1;
+//            System.out.println(view.getId());
+//            if (tabella[id] == null){
+//                tabella[id] = giocatore;
+//                vista.setText("X");
+//                giocatore = "secondo";
+//            } else{
+//                //alert che la cella è già piena
+//
+//            }
+//
+//        }else if(giocatore == "secondo"){
+//            //turno secondo giocatore
+//            Button vista = (Button) view;
+//
+//            //andiamo a decrementare id, perchè gli array partono poi da zero
+//            int id = view.getId().split("tabella").toInt() - 1;
+//            if (tabella[id] == null){
+//                tabella[id] = giocatore;
+//                vista.setText("O");
+//                giocatore = "primo";
+//            } else{
+//                //alert che la cella è già piena
+//            }
+//        }
+//
+//    }
 
-    public void click(View view) {
-        if (giocatore=="primo"){
-            giocatore = "secondo";
-            Button vista = (Button) view;
-            vista.setText("X");
-        }else if(giocatore == "secondo"){
-            //turno secondo giocatore
-            giocatore = "primo";
-            Button vista = (Button) view;
-            vista.setText("O");
-        }
+    private void controllaVittoria(){
+
     }
 }
