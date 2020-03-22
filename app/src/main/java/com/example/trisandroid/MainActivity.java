ed678dcc20240df55045bdf1bdb9b1a1f6e71dc3
matteodalgrande <matteodalgrande97@gmail.com>
2020-03-22 18:34:22 +0100

    prova stampa caratteri in bottoni

--- app/src/main/java/com/example/trisandroid/MainActivity.java
+++ app/src/main/java/com/example/trisandroid/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.trisandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String giocatore = "primo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        if (giocatore=="primo"){
+            giocatore = "secondo";
+            Button vista = (Button) view;
+            vista.setText("X");
+        }else if(giocatore == "secondo"){
+            //turno secondo giocatore
+            giocatore = "primo";
+            Button vista = (Button) findViewById(view.getId());
+            vista.setText("O");
+        }
     }
 }
