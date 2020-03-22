f52a5748b4bee2da397a6401bf4c1269a5027322
matteodalgrande <matteodalgrande97@gmail.com>
2020-03-22 21:26:54 +0100

    1)eliminazione id e aggiunta di tag nei Button dell'xml della view; 2)aggiunta dell'alert se si dovesse ripremere la stessa casella

--- app/src/main/java/com/example/trisandroid/MainActivity.java
+++ app/src/main/java/com/example/trisandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.trisandroid;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,43 +18,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void click(View view){
-        Button vista = (Button) view;
-        System.out.println(vista.getId());
-    }
-//    public void click(View view) {
-//        if (giocatore == "primo"){
-//            //turno primo giocatore
-//            Button vista = (Button) view;
-//
-//            //andiamo a decrementare id, perchè gli array partono poi da zero
-//            int id = view.getId().split("tabella").toInt() - 1;
-//            System.out.println(view.getId());
-//            if (tabella[id] == null){
-//                tabella[id] = giocatore;
-//                vista.setText("X");
-//                giocatore = "secondo";
-//            } else{
-//                //alert che la cella è già piena
-//
-//            }
-//
-//        }else if(giocatore == "secondo"){
-//            //turno secondo giocatore
-//            Button vista = (Button) view;
-//
-//            //andiamo a decrementare id, perchè gli array partono poi da zero
-//            int id = view.getId().split("tabella").toInt() - 1;
-//            if (tabella[id] == null){
-//                tabella[id] = giocatore;
-//                vista.setText("O");
-//                giocatore = "primo";
-//            } else{
-//                //alert che la cella è già piena
-//            }
-//        }
-//
+
+//    public void click(View view){
+//        Button vista = (Button) view;
+//        System.out.println(view);
+//  //      System.out.println(view.getResources().getResourceName(view.getId()));
+//        System.out.println(view.getTag());
 //    }
+    //git commit -m "1)eliminazione id e aggiunta di tag nei Button dell'xml della view; 2)aggiunta dell'alert se si dovesse ripremere la stessa casella"
+
+
+    public void click(View view) {
+        if (giocatore == "primo"){
+            //turno primo giocatore
+            Button vista = (Button) view;
+
+            //andiamo a decrementare id, perchè gli array partono poi da zero
+            int tag = Integer.parseInt((String)view.getTag());
+            if (tabella[tag] == null){
+                tabella[tag] = giocatore;
+                vista.setText("X");
+                giocatore = "secondo";
+            } else{
+                //alert--> la cella è già piena
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Mossa non concessa");
+                alertDialog.setMessage("Questa casella è già stata selezionata. E' necessario selezionare una casella vuota");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                alertDialog.show();
+            }
+
+        }else if(giocatore == "secondo"){
+            //turno secondo giocatore
+            Button vista = (Button) view;
+
+            //andiamo a decrementare id, perchè gli array partono poi da zero
+            int tag = Integer.parseInt((String)view.getTag());
+            if (tabella[tag] == null){
+                tabella[tag] = giocatore;
+                vista.setText("O");
+                giocatore = "primo";
+            } else{
+                //alert--> la cella è già piena
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Mossa non concessa");
+                alertDialog.setMessage("Questa casella è già stata selezionata. E' necessario selezionare una casella vuota");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        }
+
+    }
 
     private void controllaVittoria(){
 
