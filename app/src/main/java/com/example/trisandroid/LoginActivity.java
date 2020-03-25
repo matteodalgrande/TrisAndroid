934ee43289df89528577e9bb9b93b55f80f2c4e4
matteodalgrande <matteodalgrande97@gmail.com>
2020-03-25 23:12:57 +0100

    prova firebase database funzionante per Multiplayer

--- app/src/main/java/com/example/trisandroid/LoginActivity.java
+++ app/src/main/java/com/example/trisandroid/LoginActivity.java
@@ -13,13 +13,15 @@ public class LoginActivity extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.login_main);
         }
-        public void cambiaSchermata(View view){
+        public void cambiaSchermata(View view) {
             Intent myIntent = new Intent(this, MainActivity.class);
             EditText editText1 = (EditText) findViewById(R.id.editText1);
             EditText editText2 = (EditText) findViewById(R.id.editText2);
-            System.out.println("login-------------->"+editText1.getText());
+            System.out.println("login-------------->" + editText1.getText());
             myIntent.putExtra("giocatore1", editText1.getText().toString());//BISOGNA FARLO DIVENTARE UN STRINGA IN QUALCHE MODO!
             myIntent.putExtra("giocatore2", editText2.getText().toString());
             this.startActivity(myIntent);
         }
+
+
 }
